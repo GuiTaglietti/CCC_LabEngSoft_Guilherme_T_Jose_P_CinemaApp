2555c54509060fe95b977e7b5cfa667f804a1343
José Paulo Remonti Pereira <128315489+JosePRP1@users.noreply.github.com>
2025-05-16 15:35:48 -0300

    Delete maintenanceService.js

--- maintenanceService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export async function sendMaintenanceMessage(sender, content) {
-  return await axios.post('http://localhost:5000/api/maintenance/messages/', {
-    sender,
-    content,
-    date: new Date().toISOString()
-  });
-}
